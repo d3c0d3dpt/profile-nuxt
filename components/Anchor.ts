2e7dc7ad8ea862445a3f516df2a843832ae455c0
alvaro.ferreira <alvaro.ferreira@jumia.com>
2021-07-29 19:25:17 +0100

    added rel noreferrer to anchors

--- components/Anchor.ts
+++ components/Anchor.ts
@@ -6,12 +6,26 @@ export default {
         return createElement(
             'a',
             {
-                attrs: { ...context.data.attrs, class: 'text-indigo-800 text-sm', target: '_blank' }
+                attrs: {
+                    ...context.data.attrs,
+
+                    class: 'text-indigo-800 text-sm',
+                    rel: 'noreferrer',
+                    target: '_blank'
+                }
             },
             [
                 context.children,
 
-                createElement('external-link-icon', { attrs: { class: 'inline align-text-top ml-0.5', size: 8 } })
+                createElement(
+                    'external-link-icon',
+                    {
+                        attrs: {
+                            class: 'inline align-text-top ml-0.5',
+                            size: 8
+                        }
+                    }
+                )
             ]
         );
     }
