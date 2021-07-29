5a91cc080e2db79e196290316eef1b8eb0f163a0
alvaro.ferreira <alvaro.ferreira@jumia.com>
2021-07-29 19:05:33 +0100

    fixed static compilation issue

---
+++ components/Anchor.ts
@@ -0,0 +1,18 @@
+export default {
+    functional: true,
+
+    // @ts-ignore
+    render (createElement, context) {
+        return createElement(
+            'a',
+            {
+                attrs: { ...context.data.attrs, class: 'text-indigo-800 text-sm', target: '_blank' }
+            },
+            [
+                context.children,
+
+                createElement('external-link-icon', { attrs: { class: 'inline align-text-top ml-0.5', size: 8 } })
+            ]
+        );
+    }
+};
